d60bb9fafa871f76c806b7bd887587382b4ecbe8
Navjeet Kaur <kaurnavjeet09@gmail.com>
2023-08-20 00:12:06 -0700

    Fixed Label Tkinter

--- main.py
+++ main.py
@@ -12,19 +12,21 @@ customtkinter.set_default_color_theme("blue")
 app = customtkinter.CTk()
 app.geometry("600x440")
 
+output_label = customtkinter.CTkLabel(master=app, text="", font=("Arial", 18))
+
 
 def random_password_generator():
     random_password = ""
     characters = string.ascii_letters + string.digits + string.punctuation
     for i in range(13):
         random_password += ''.join(random.choice(characters))
-    label.config(textvariable=random_password)
+    output_label.configure(text=random_password)
 
 
-button = customtkinter.CTkButton(master=app, text="Generate Random Password", command=random_password_generator(),
+button = customtkinter.CTkButton(master=app, text="Generate Random Password", command=random_password_generator,
                                  border_spacing=10)
 button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
-label = customtkinter.CTkLabel(master=app, textvariable=random_password_generator())
+output_label.place(relx=0.5, rely=0.3, anchor=customtkinter.CENTER, )
 
 app.mainloop()
