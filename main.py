ff318df223855dd6acf43e4361b9be458060b1e9
Navjeet Kaur <kaurnavjeet09@gmail.com>
2023-08-20 22:14:41 -0700

    Created readme and fixed minor GUI issues

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import random
 import string
 
 # Create a random password generator
-# On clicking generate, create a 8-12 character long password
-#  contains at least 1 special character and 1 number
+# On clicking generate, create  a 12 character long password
+# contains at least 1 special character and 1 number
+
 
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 
-app = customtkinter.CTk()
-app.geometry("600x440")
+root = customtkinter.CTk()
+root.geometry("500x340")
 
-output_label = customtkinter.CTkLabel(master=app, text="", font=("Arial", 18))
+output_label = customtkinter.CTkLabel(master=root, text="", font=("Arial", 18))
 
 
 def random_password_generator():
@@ -23,10 +24,10 @@ def random_password_generator():
     output_label.configure(text=random_password)
 
 
-button = customtkinter.CTkButton(master=app, text="Generate Random Password", command=random_password_generator,
+button = customtkinter.CTkButton(master=root, text="Generate Random Password", command=random_password_generator,
                                  border_spacing=10)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+button.place(relx=0.5, rely=0.6, anchor=customtkinter.CENTER)
 
-output_label.place(relx=0.5, rely=0.3, anchor=customtkinter.CENTER, )
+output_label.place(relx=0.5, rely=0.4, anchor=customtkinter.CENTER, )
 
-app.mainloop()
+root.mainloop()
---
+++ readme.txt
@@ -0,0 +1,10 @@
+# Random Password Generator
+- After clicking on the Generate Password button, a random password that is 12 characters long will be shown.
+Password is:
+1. At least 12 characters long
+2. Is alphanumerical.
+3. Has special characters
+* Note: Not safe to use in production
+
+App made with Python and Tkinter.
+
